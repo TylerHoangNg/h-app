1146f56bbbb257973a5fb5c718a68e35b355ec74
Nguyễn Huy Hoàng <94150981+TylerHoangNg@users.noreply.github.com>
2021-12-11 16:03:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -53,8 +53,7 @@ app.use('/api/auth', authRouter)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/public'))
     app.get('*', (req, res) => {
-        // res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
-        res.sendFile('client/public/index.html'); 
+        res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
     })
 }
 
